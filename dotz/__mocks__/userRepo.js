e11e4e4b0ec66fb883465b0e5f70f886e86fc16c
Karthik Venkataraman <karthik.venkataraman@metrosystems-external.net>
2016-02-27 18:26:16 +0100

    Adding test for userRepo

--- dotz/__mocks__/userRepo.js
+++ dotz/__mocks__/userRepo.js
@@ -1,7 +1,13 @@
 /*
 Mocked version of userRepo.js for Jest testing
 */
-jest.dontMock('../src/');
+jest.dontMock('../src/backend/repo/userRepo.js');
 var User = require('../models/User');
 var cassandraClient = require('../dbClient');
 
+describe('sum', function() {
+ it('Has Vishal logged in', function() {
+   var user = require('../sum');
+   expect(userRepo('Vishal', null)).toBe(3);
+ });
+});
